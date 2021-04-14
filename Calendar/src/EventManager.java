ba5b98d6cfd6aa32f0bdc2e7b8037cfb8a71b9e2
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-04-14 14:15:24 -0500

    Update EventManager.java

--- Calendar/src/EventManager.java
+++ Calendar/src/EventManager.java
@@ -1,6 +1,24 @@
-public interface EventManager {
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.util.Calendar;
 
-    public void setLocation();
-    public String display ();
+public class EventManager {
+	Events event ;
+	Scanner sc;
+	
+	public void setDuration(Duration duration) {
+		// take any amount of time of Duration subclass
+	}
+	
+	public void createSingleEvent() { // or might need to return EVENT
+		System.out.println("Enter the name of the event you are initiating.");
+		String eventName = sc.nextLine();
+		
+		System.out.println("Enter the date and time you for you event.");
+		LocalDate eventDate = new LocalDate(2, 0, 0)
+		
+	}
+	
+	
 
 }
