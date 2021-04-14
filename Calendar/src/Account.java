bcfa21f06297c6bd224024fb828fbbf79e02985b
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-04-13 21:41:42 -0500

    Update Account.java
    added new fields

--- Calendar/src/Account.java
+++ Calendar/src/Account.java
@@ -1,29 +1,80 @@
 import java.util.Calendar;
 
 public class Account {
-    private String name;
-    private String password;
-    private String email;
-    Schedule personalSchedule;
-    Calendar personalCalendar;
+	private String name;
+	private String email;	
+	private String password;
+	private String title;
+	Calendar calendar;
+	Schedule schedule;
+	private String calendarUserName;
+	
+	
+
+	Account(String fullName, String email, String password) {
+		this.name = fullName;
+		this.email = email;
+		this.password = password;
+		calendar = new Calendar();
+		calendarUserName = setCalendarUserName();
+		schedule= new Schedule();
+		
+	}
+	
+	public String getPassword() {
+		return password;
+	}
 
 
 
-
-     Account (String name,String email,String password){
-         this.name = name;
-         this.password = password;
-         this.email = email;
-
-    }
+	public void resetPassword(String password) {
+		this.password = password;
+	}
 
 
-    public String checkEmail (String email){
-         if ( email.contains("@")){
-             return email;
 
-         }else {
-             return null;
-         }
-    }
+	public String getTitle() {
+		return title;
+	}
+
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Calendar getCalendar() {
+		return calendar;
+	}
+	public void setCalendar(Calendar calendar) {
+		this.calendar = calendar;
+	}
+
+	public String getCalendarUserName() {
+		return calendarUserName;
+	}
+
+	public String setCalendarUserName() {
+		String userName =this.title + this.name; /// 
+		
+		
+		return userName;
+	}
+	
+	public String toString() {
+		return "User name :"+ name+ "\n "
+			+ " user email :"+ email+ "\n "
+			+ " Calendar user ID: "+ calendarUserName +"\n "
+			+ " User schedule: "+ schedule.toString();
+	}
 }
