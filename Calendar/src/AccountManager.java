3be8cd41b382f9260457bf6b401b0ddffaeee14a
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-04-13 19:25:29 -0500

    Updated AccountManager.java
    Added menu;
    created method for login/create account/reset password queries.

--- Calendar/src/AccountManager.java
+++ Calendar/src/AccountManager.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 
 public class AccountManager {
 	
-	Scanner sc = new Scanner(System.in);
-	
+	private Scanner sc = new Scanner (System.in);
+	private LinkedList<Account> accountsList= new LinkedList<>();
+	EmailCheck emailCheck ;
+	Account account ;
 	
 	public void run() {
 		 displayMenu();
+		 
 		 int answer = sc.nextInt();
+		 
 		 switch (answer) {
 		 case 1: logIn();
 		 break;
@@ -15,36 +19,70 @@ public class AccountManager {
 	 	 break;
 	 	 case 3: resetPassword();
 	 	 break;		
-	 		
+ 		
 		 } 
-		run();
+	
 	}
 	
-		 
-		 
-	
-	 
 	 public void displayMenu() {
 		 
 	 System.out.println("Press any number from the menu below to prosses through out the application.");
 		 System.out.println("1.Log in.");
-		 System.out.println("2.Create anaccount");
+		 System.out.println("2.Create an account");
 		 System.out.println("3.Reset password");
 		 
 	 }
-	 
+ 
 	 public void logIn() {
-		 // connect the method with an account method and creat account as a user object with all atributes
+		 
+		Scanner sr = new Scanner (System.in);
+		System.out.println(" Enter email.");
+		String email =  sr.next();
+		
+		//emailCheck.checkEmail(email);
+		 
+		System.out.println("Enter you password.");
+		String password = sr.next();
+		
+		account = findByEmail(email);
+		 
+		if (account.getPassword().equals(password)) {
+			 // display user account
+			account.toString();
+		}	
 	 }
+	
 	 
 	 public void createAccount() {
+		 System.out.print(" Enter your full name.\n");
+		 String fullName = sc.next();
+		 		 
+		 System.out.print(" Enter your email.\n");
+		 String email = sc.nextLine();
 		 
+		//emailCheck.checkEmail(email);
+		 System.out.print(" Enter your password.");
+		 String password = sc.next();
+		 
+		 
+		 Account ac = new Account (fullName,email,password);
+		 accountsList.add(ac); 
+		 System.out.println( ac.toString());
+
+		 run();
 	 }
+	 
 	 public void resetPassword() {
+		 System.out.println("Enter you email.");
+		 String email = sc.next();
 		 
-	 }
-
-	
-
-} 
+		 System.out.println("Enter your new password");
+		 String newPassoword = sc.next();
+	 
+	 }		 
+	 
+	 	Account findByEmail(String email) {
+		    return accountsList.stream().filter(account -> email.equals(account.getEmail())).findFirst().orElse(null);
+		}
+}
 
