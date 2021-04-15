9eff38c6677ce868b0cb61ef5ff8b60224d73a28
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-04-14 20:23:46 -0500

    Update Schedule.java

--- Calendar/src/Schedule.java
+++ Calendar/src/Schedule.java
@@ -1,7 +1,35 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Schedule {
-    List<Event> events = new ArrayList();
-    List <Availibility> workhours = new ArrayList();
-}
+ublic class Schedule  {
+	ScheduleManager scheduleManager;
+	private Availability time;
+	private HashMap<Time, Events> events = new HashMap<>();
+	
+	public Schedule() {
+		
+	}
+	
+	
+	
+	
+	public HashMap<Time, Events> getEvents() {
+		return events;
+	}
+
+
+
+	public void setEvents(HashMap<Time, Events> events) {
+		this.events = events;
+	}
+
+
+	public Availability getTime() {
+		return time;
+	}
+
+
+
+	public void setTime(Availability time) {
+		this.time = time;
+	}
